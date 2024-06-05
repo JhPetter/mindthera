21fdd68b5dcb837544146e74480ea5915c2c1c50
Petter Infante <petter.infante@macys.com>
2024-06-04 23:03:47 -0500

    add a description to the website

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <p>Hello World Petter!</p>
+      <p>This is a project to handle the appointments for the web page</p>
     </div>
   );
 }
